d6458c937d903a520796814b1e33fdb746e16280
Jaime López López <jaloplo@gmail.com>
2018-01-24 22:27:50 +0100

    + Added keyboard support.

---
+++ src/OpenCol/KeyboardHandler.cs
@@ -0,0 +1,42 @@
+namespace OpenCol {
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Input;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class KeyboardHandler {
+
+        public Action<IList<Keys>> KeyDown;
+        public Action<IList<Keys>> KeyUp;
+
+        protected Keys[] LastKeysPressed { get; set; }
+
+        public KeyboardHandler() {
+        }
+
+        public virtual void Update(GameTime gameTime) {
+            
+            var state = Keyboard.GetState();
+            var currentKeysPressed = state.GetPressedKeys();
+            
+
+            // KeyDown process
+            var newKeysPressed = currentKeysPressed.Where(k => !LastKeysPressed.Contains(k)).ToList();
+
+            if(null != KeyDown)
+                KeyDown(newKeysPressed);
+
+
+            // KeyUp process
+            var oldKeysPressed = LastKeysPressed.Where(k => !currentKeysPressed.Contains(k)).ToList();
+
+            if(null != KeyUp)
+                KeyUp(oldKeysPressed);
+
+
+            LastKeysPressed = currentKeysPressed;
+        }
+    }
+}
--- src/OpenCol/OpenColGame.cs
+++ src/OpenCol/OpenColGame.cs
@@ -9,6 +9,8 @@ namespace OpenCol.Client {
         public GraphicsDeviceManager Graphics { get; private set; }
         protected SpriteBatch SpriteBatch { get; set; }        
 
+        protected KeyboardHandler Keyboard { get; private set; }
+
         public OpenColGame() {
             Window.Title = "OpenCol Game";
             Window.Position = new Point(100, 100);
@@ -18,21 +20,27 @@ namespace OpenCol.Client {
             Graphics = new GraphicsDeviceManager(this);
 
             Graphics.ApplyChanges();
+
+            Keyboard = new KeyboardHandler();
         }
 
         protected override void Initialize() {
             base.Initialize();
 
+            // TODO: Creates a renderer.
             SpriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
         protected override void LoadContent() {
             base.LoadContent();
+
+            // TODO: Load all tipes of tiles that will be drawn in the screen.
         }
 
         protected override void Draw(GameTime gameTime) {
             Graphics.GraphicsDevice.Clear(Color.Bisque);
 
+            // TODO: Use renderer class for drawing everything.
             SpriteBatch.Begin();
             SpriteBatch.End();
 
@@ -40,6 +48,8 @@ namespace OpenCol.Client {
         }
 
         protected override void Update(GameTime gameTime) {
+            Keyboard.Update(gameTime);
+
             base.Update(gameTime);
         }
     }
